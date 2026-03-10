44ccf94a09000a3692f973249b5d08ce7698ac68
Tirtharaj Pramanik <tbytes.404@icloud.com>
2026-03-10 17:54:57 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,74 +2,67 @@
 use std::io::{self, Write};
 use std::{
     env::{current_dir, set_current_dir, split_paths, var_os},
-    fs::read_dir,
-    ops::Deref,
-    path::Path,
+    path::{Path, PathBuf},
     process::Command,
 };
 
 use is_executable::IsExecutable;
 
 const COMMAND_PROMPT: &str = "$ ";
-const BUILTIN_COMMANDS: [&str; 4] = ["exit", "echo", "type", "pwd"];
+const BUILTIN_COMMANDS: [&str; 5] = ["exit", "echo", "type", "pwd", "cd"];
 
 fn main() {
     loop {
         show_prompt(COMMAND_PROMPT);
-        let (cmd, args) = read_command();
-        match cmd.deref() {
+        let input = read_command();
+        let (cmd, args) = first_arg(&input);
+
+        match cmd {
             "exit" => break,
             "echo" => println!("{}", args),
+            "type" => builtin_type(args),
             "pwd" => println!("{}", current_dir().unwrap().display()),
-            "cd" => {
-                let arg = match args.split_once(" ") {
-                    Some((arg, _)) => arg,
-                    _ => args.as_str(),
-                };
-                let path = Path::new(arg);
-                if path.exists() {
-                    set_current_dir(path).unwrap();
-                } else {
-                    println!("cd: {}: No such file or directory", arg)
-                }
-            }
-            "type" => {
-                let arg = match args.split_once(" ") {
-                    Some((arg, _)) => arg,
-                    _ => args.as_str(),
-                };
-                if BUILTIN_COMMANDS.contains(&arg) {
-                    println!("{} is a shell builtin", arg)
-                } else if let Some(path) = find_executable(arg) {
-                    println!("{} is {}", arg, path)
-                } else {
-                    println!("{}: not found", arg)
-                }
-            }
-            cmd => {
-                if find_executable(cmd).is_some() {
-                    let output = Command::new(cmd).args(args.split(" ")).output().unwrap();
-                    print!("{}", String::from_utf8_lossy(&output.stdout))
-                } else {
-                    println!("{}: command not found", cmd)
-                }
-            }
+            "cd" => builtin_cd(args),
+            exe => run_executable(exe, args),
         }
     }
 }
 
-fn find_executable(arg: &str) -> Option<String> {
-    let paths = { var_os("PATH").unwrap() };
-    for path in split_paths(&paths).filter(|p| p.exists()) {
-        for entry in read_dir(path).unwrap() {
-            let entry = entry.unwrap().path();
-            let file = entry.file_name().unwrap();
-            if file == arg && entry.is_executable() {
-                return Some(entry.display().to_string());
-            }
-        }
+fn run_executable(cmd: &str, args: &str) {
+    if find_executable(cmd).is_some() {
+        let output = Command::new(cmd).args(args.split(" ")).output().unwrap();
+        print!("{}", String::from_utf8_lossy(&output.stdout))
+    } else {
+        println!("{}: command not found", cmd)
     }
-    None
+}
+
+fn builtin_cd(args: &str) {
+    let (arg, _) = first_arg(args);
+    let path = Path::new(arg);
+    if path.exists() {
+        set_current_dir(path).unwrap();
+    } else {
+        println!("cd: {}: No such file or directory", arg)
+    }
+}
+
+fn builtin_type(args: &str) {
+    let (arg, _) = first_arg(args);
+    if BUILTIN_COMMANDS.contains(&arg) {
+        println!("{} is a shell builtin", arg)
+    } else if let Some(path) = find_executable(arg) {
+        println!("{} is {}", arg, path.display())
+    } else {
+        println!("{}: not found", arg)
+    }
+}
+
+fn find_executable(arg: &str) -> Option<PathBuf> {
+    let paths = { var_os("PATH").unwrap() };
+    split_paths(&paths)
+        .map(|dir| dir.join(arg))
+        .find(|p| p.is_executable())
 }
 
 fn show_prompt(sym: &str) {
@@ -77,13 +70,15 @@ fn show_prompt(sym: &str) {
     io::stdout().flush().unwrap();
 }
 
-fn read_command() -> (String, String) {
+fn read_command() -> String {
     let mut line = String::new();
     io::stdin().read_line(&mut line).unwrap();
-    let line = line.trim();
+    line.trim().to_string()
+}
 
-    match line.split_once(" ") {
-        Some((cmd, args)) => (cmd.into(), args.into()),
-        None => (line.into(), "".into()),
+fn first_arg(args: &str) -> (&str, &str) {
+    match args.split_once(|c: char| c.is_whitespace()) {
+        Some((f, e)) => (f, e.trim_start()),
+        None => (args, ""),
     }
 }
